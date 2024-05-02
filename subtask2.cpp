a8d50001d5597a79ba0eeeeb66f87271061a470e
Mseleku ntombizethu <167969729+9906110610085@users.noreply.github.com>
2024-05-02 11:30:06 +0200

    temperature reading
    you may increase and decrease the fryer heat

---
+++ subtask2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	int tempreading1;
+	int tempreading2;
+	int tempreading3;
+	int tempdifference;
+	
+	cout << "Enter the first temperature reading" <<endl;
+	cin>> tempreading1;
+	
+	cout << "Enter the second temperature reading"<<endl;
+	cin >> tempreading2;
+	
+	
+	
+	tempdifference = tempreading2 - tempreading1
+	if (tempdifference > 50);
+	{
+	
+		cout << "Reduce fryer heat before taking the third reading"<<endl;
+		}
+	else if (temperatue <10);
+	{
+		cout << "Ïncrease fryer heat before taking the third reading" <<endl;
+		}
+	cout << "Ënter the third temperature reading"<<endl;
+	cin >> tempreading3;
+	
+	if (tempetature 3>150 :tempreading3< 190);
+	
+	{
+		cout << "You may start frying gwinyas" <<endl;
+		}
+		
+	else
+	{
+		cout<< "Oil is not ready for frying"<<endl;
+		}
+
+	return 0;
+	
+	}
